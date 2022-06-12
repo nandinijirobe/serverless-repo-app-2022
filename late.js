f41aa7b10dd04f07a65a0be05ed12eb37ee03a61
njirobe <njiro2@uic.edu>
2022-06-12 12:00:33 -0500

    running_late function added

--- late.js
+++ late.js
@@ -1,1 +1,16 @@
-// start coding your function here!
+// This is the running late function
+// It returns late if time is past 10pm
+// It returns early if time is before 10pm
+
+function running_late(date){
+    // console.log(date.getHours());
+    if (date.getHours() >= 22) {
+        return "It is late!";
+    } else {
+        return "It is still early!";
+    }
+} 
+
+someDate = new Date('December 17, 1995 23:24:00');
+// console.log(running_late(someDate));
+module.exports = running_late
