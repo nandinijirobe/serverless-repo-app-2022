e83f2c40a2bb4bad2c312e70a3addff2fb4442fa
njirobe <njiro2@uic.edu>
2022-06-12 14:34:48 -0500

    max function added

--- max_nums.js
+++ max_nums.js
@@ -1,1 +1,14 @@
 // start coding your function here!
+function find_max(arr) {
+    max = arr[0];
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i] > max) {
+            max = arr[i];
+        }
+    }
+    return max;
+}
+
+// numbers = [1,2,3,4,5,7,6];
+// console.log(find_max(numbers));
+exports.find_max = find_max;
