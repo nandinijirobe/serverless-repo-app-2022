19050bbcad22adbf8a504bb61ff237260e76f1c2
njirobe <njiro2@uic.edu>
2022-06-11 22:45:09 -0500

    added hello world export function

--- helloworld.js
+++ helloworld.js
@@ -1,1 +1,8 @@
+// I am trying to commit changes here
 // code your `hello` function here!
+function hello() {
+    // console.log ("Hello World");
+    return "Hello World";
+} 
+// console.log(hello());
+module.exports = hello;
